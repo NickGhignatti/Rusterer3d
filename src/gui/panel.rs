fc779a5058fbbf14511be26d62b4ea73381c3642
NickGhignatti <nick.ghignatti@gmail.com>
2025-10-17 20:39:08 +0200

    Fix typo in commit

--- src/gui/panel.rs
+++ src/gui/panel.rs
@@ -8,8 +8,8 @@ pub fn create_panel(app: &mut Renderer3DApp, ctx: &egui::Context) {
         
         let available_rect = ui.available_rect_before_wrap();
         ui.scope_builder(UiBuilder::new().max_rect(available_rect), |ui| {
-            // This would be where you integrate your 3D rendering
-            // For now, we'll just show a placeholder
+            // TODO
+            // Integrate 3D rendering
             let painter = ui.painter();
 
             // Draw background
@@ -19,7 +19,6 @@ pub fn create_panel(app: &mut Renderer3DApp, ctx: &egui::Context) {
                 egui::Color32::from_gray(45),
             );
 
-            // Draw simple representation of objects
             if let Some(selected_idx) = app.selected_object
                 && selected_idx < app.scene_objects.len()
             {
